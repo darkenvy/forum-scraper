e6f6f31daa4496e23a802549884f1bc97436e766
darkenvy <darkenvy6@gmail.com>
2016-10-22 21:55:15 -0700

    Ajax from Tampermonkey works

--- font-end.js
+++ font-end.js
@@ -1,20 +1,28 @@
 var getThreads = function() {
   var allThreads = $('a.group-thread__link');
-  var cleaned = {}
+  var cleaned = {};
   for (var i=0; i<allThreads.length; i++) {
     var key = allThreads[i].href.split('?')[1];
     cleaned[key] = allThreads[i].children[4].innerText;
-  };
+  }
   return cleaned;
 };
 
-
-$.ajax({
-  type: "POST",
-  url: "localhost:3000",
-  data: getThreads,
-  // success: success
-  // dataType: "json"
-}).done(function(res) {
-  console.log(res);
-})
+console.log(getThreads());
+if ($('a.group-thread__link').length > 0) {
+  GM_xmlhttpRequest({
+    method: "POST",
+    url: "http://127.0.0.1:3000",
+    data: JSON.stringify(getThreads()),
+    // data: getThreads(),
+    // headers: {"Content-Type": "application/x-www-form-urlencoded"},
+    headers: {"Content-Type": "application/json; charset=utf-8"},
+    dataType: "json",
+    onload: function(response) {
+      console.log(response);
+      aux();
+    }
+  });
+} else {
+  aux();
+}
--- index.js
+++ index.js
@@ -2,19 +2,8 @@ var express = require('express');
 var db = require('./models');
 var bodyParser = require('body-parser');
 var app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(function(req, res, next) {
-//   // Website you wish to allow to connect
-//       res.setHeader('Access-Control-Allow-Origin', '*');
-
-//       // Request methods you wish to allow
-//       // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//       // Request headers you wish to allow
-//       // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// });
 
 // Function to turn British format into American then Unix
 var convertTime = function(timeString) {
@@ -27,36 +16,42 @@ var convertTime = function(timeString) {
 // ––––––––––––––––––––––––– Routes –––––––––––––––––––––––– //
 
 app.post('/', function(req, res) {
-  res.send(JSON.stringify(req.body));
+  
+  var currentTime = parseInt(new Date().valueOf()/1000),
+      times = req.body;
+  // Look at all the incoming times. If the time happened within 10 minutes,
+  // then we can update our model. We do this to lower DB access
+  for (var key in times) {
+    var timeDiff = currentTime - convertTime(times[key]) // key 387,388 // times[key] 19/10/16 09:24
+    if (timeDiff <= 600) { updateTopLevel(key, times[key]); } // If the thread is newer then 10 minutes, lets update our model!
+  }
+  console.log('received POST at', new Date());
+  res.send('Done');
 });
 
-app.post('/top', function(req, res) {
+// ––––––––––––––––––––– DB Functions –––––––––––––––––––– //
 
+function updateTopLevel(id, time) {
   // Prepare the object to pass into the DB.
   var findOrCreateObj = {
-    where: {threadId: req.query.id},
-    defaults: {threads: req.query.threads, posts: req.query.posts, latest: convertTime(req.query.latest) } 
+    where: {threadId: id},
+    defaults: {latest: convertTime(time)} 
   }
-
-  // This is the bulk of the work.
   // If the forum model is older than new information, update model, alert users
   var spread = function(user, created) {
-    if (!created && user.latest < convertTime(req.query.latest)) {
-      user.threads = req.query.threads;
-      user.posts = req.query.posts;
-      user.latest = convertTime(req.query.latest);
+    if (!created && user.latest < convertTime(time)) {
+      console.log('----------------------- UPDATED: ', user.threadId, '-----------------');
+      user.latest = convertTime(time);
       user.save();
     }
   }
-
   // The actual DB commands. Cleaned up for easy readability.
   db.topLevel
     .findOrCreate(findOrCreateObj)
     .spread(function(user, created) {spread(user, created);})
     .error(function(err) {console.log('An error occurred in FindOrCreate : ', error); })
-    .then(function() {res.send('success'); });
-
-});
+};
 
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––– //
+
 var server = app.listen(process.env.PORT || 3000)
